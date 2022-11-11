eaf2f446b43b700bd534a746fa0a7d0955f02e1c
usan31 <usan31showtek@gmail.com>
2022-11-10 21:23:04 -0600

    Aun preparando los chingados Rooms, y progreso

--- application.py
+++ application.py
@@ -1,83 +1,74 @@
 import secrets
 
-from flask import Flask, render_template, redirect, request, session, flash, g
+from flask import Flask, render_template, redirect, request, session, flash
 from flask_socketio import SocketIO, emit, join_room, leave_room
 from login_required import login_required
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = secrets.token_hex()
-socketio = SocketIO(app)
+socketio = SocketIO(app, cors_allowed_origins='*')
 
-g.channels = ["General"]
+channels = ["General"] 
+messages = {channels[0] : []}
 
 @app.route("/")
 @login_required
 def index():
-    print(f"la lista de channels carga y esto es lo  que tiene {channels}")
-    join_room(channels[0])
-    return render_template("index.html", username=session["username"], current_channel=g.channels[0])
-
-
-@app.route("/newChannel", methods=["POST","GET"])
-def new_channel():
-    lower_channel = []
-    name_channel = request.form.get("newChannel")
-    for channel in g.channels:
-        lower_channel.append(channel.lower())
-
-    if name_channel.lower() in lower_channel:
-        flash("This name is alraedy in use")
-        lower_channel = []
-        return redirect("/")
-    else:
-        g.channels.append(name_channel)
-        lower_channel = []
-        join_room(name_channel)
-        return render_template("index.html", current_channel=name_channel)
-
+    print(f"El canal actual es {session['current_channel']}")
+    return render_template("index.html", username=session["username"]) 
 
 @app.route("/login", methods=["GET", "POST"])
 def login():
     if request.method == "POST":
         username = request.form.get("username")
         session["username"] = username
+        session["current_channel"] = "General"
         return redirect("/")
     else:
         return render_template("login.html")
-
 
 @app.route("/logout")
 def logout():
     session.clear()
     return redirect("/login")
 
+
 @socketio.on('message')
-def handle_data(msg, username, date):
+def message(msg, username, date):
     print(msg, username, date)
-    emit("messageOut", (msg, username, date), broadcast=True)
+    join_room(session["current_channel"])
+    emit('messageOut', (msg, username, date, session["current_channel"]), broadcast=True, to=session["current_channel"])
+    emit('test', {"keytest" : "datatest"})
+
+@socketio.on("create_channel")
+def new_channel(channel):
+    lower_channel = []
+    for channel in channels:
+        lower_channel.append(channel.lower())
+
+    if channel.lower() in lower_channel:
+        flash("This name is alraedy in use")
+    else:
+        channels.append(channel)
+        leave_room(session["current_channel"])
+        emit("newChannel", channel, broadcast=True) 
+
+    lower_channel = []
 
 @socketio.on("entrar")
 def entrar(sala):
     # Ingresamos al usuario a la sala
-    print(sala)
+    leave_room(session["current_channel"])
     join_room(sala)
 
     # Emitimos un aviso a los usuarios conectados a la sala, exceptuando a la persona que se unio
     emit('mensaje', f'Un usuario ha entrado a la sala {sala}', broadcast=True, include_self=False, to=sala)
-
+    
+    emit('visibilityMsg', (sala, session["current_channel"])) 
     # Mandamos una respuesta al evento emit del cliente
+    session["current_channel"] = sala 
     return f'Te has unido a la sala {sala}'
 
-@socketio.on("salir")
-def salir(sala):
-    # Ingresamos al usuario a la sala
-    leave_room(sala)
-
-    # Emitimos un aviso a los usuarios conectados a la sala, exceptuando a la persona que se unio
-    emit('mensaje', f'Un usuario ha salido a la sala {sala}', broadcast=True, include_self=False, to=sala)
-
-    # Mandamos una respuesta al evento emit del cliente
-    return f'Te has salido a la sala {sala}'
 
 if __name__ == "__main__":
     socketio.run(app)
--- static/js/script.js
+++ static/js/script.js
@@ -1,23 +1,25 @@
 // Event name of send in SocketIO is "message"
 const socket = io();
-var username, msgIn, msgOut;
 
+// Send the messages 
 document.querySelector("#send").onclick = () => {
-    if (document.querySelector("#name").value) {
-        var date = new Date();
-        var dateMsg = `${date.getDate()}/${date.getMonth()} ${date.getHours()}:${date.getMinutes()}min` 
-        username = document.querySelector("#name").value;
-        msgIn = document.querySelector(".msg").value;
-        socket.send(msgIn, username, dateMsg);
-        document.querySelector(".msg").value = "";
-    } else {
-        alert("Para enviar mensajes necesitas insertar un nombre de usuario")
-    }
+    var date = new Date();
+    var dateMsg = `${date.getDate()}/${date.getMonth()} ${date.getHours()}:${date.getMinutes()}min`;
+    username = document.querySelector("#username").value;
+    msgIn = document.querySelector(".msg").value;
+    socket.send(msgIn, username, dateMsg);
+    document.querySelector(".msg").value = "";
 };
 
-socket.on("messageOut", function (msgOut, username, dateMsg) {
+socket.on("test", function (data) {
+    alert(`Se EJECUTO test con ${data}`);
+});
+
+// View the messages
+socket.on("messageOut", function (msgOut, username, dateMsg, currentChannel) {
+    alert("SE EJECUTO EL messageOut");
     document.querySelector("#chat").innerHTML += (
-        `<li class='list-group-item d-flex justify-content-between align-items-start'>
+        `<li class='${currentChannel} list-group-item d-flex justify-content-between align-items-start'>
             <div class="ms-2 me-auto">
                 <div class="fw-bold">${username}</div>
                 ${msgOut}
@@ -26,54 +28,34 @@ socket.on("messageOut", function (msgOut, username, dateMsg) {
         </li>`);
 });
 
-// respuesta = Es el mensaje de estado, solo se muestra para 
-// el propio Usuario
-/*
-document.querySelector("#boton1").onclick = () => {
-    socket.emit("entrar", "CS50x.ni", (respuesta) => {
-        document.querySelector("#contenido").append(respuesta)
-        document.querySelector("#contenido").innerHTML += "<br>"
-        document.querySelector("#msg").className = "CS50x.ni"
-        document.querySelector("#salir1").removeAttribute("hidden")
-    })
-}
+// Create new channel 
+document.querySelector(".createChannel").onclick = () => {
+    if (document.querySelector(".newChannel").value) {
+        newChannel = document.querySelector(".newChannel").value;
+        socket.emit("create_channel", newChannel);
+        document.querySelector(".newChannel").value = "";
+    } else {
+        alert("Para crear un chat necesitas insertar un nombre");
+    }
+};
 
-document.querySelector('#boton2').onclick = () => {
-    socket.emit("entrar", "Web50x.ni", (respuesta) => {
-        console.log(respuesta)
-        document.querySelector("#contenido").append(respuesta)
-        document.querySelector("#contenido").innerHTML += "<br>"
-        document.querySelector("#msg").className = "Web50x.ni"
-        document.querySelector("#salir2").removeAttribute("hidden")
-    })
-}
+// Show the new channel
+socket.on("newChannel", function (channel) {
+    document.querySelector(".channels-list").innerHTML += (
+       `<li class="nav-item channel">
+            <a class="nav-link" href="#">${channel}</a>
+        </li>`
+    );
+});
 
-document.querySelector("#salir1").onclick = () => {
-    socket.emit("salir", "CS50x.ni", (respuesta) => {
-        document.querySelector("#contenido").append(respuesta)
-        document.querySelector("#contenido").innerHTML += "<br>"
-        document.querySelector("#msg").className = ""
-        document.querySelector("#salir1").setAttribute("hidden", true)
-    })
-}
+// Enter to channel
+document.querySelector(".channel").onclick = () => {
+    enterChannel = document.querySelector(".channel").value;
+    socket.emit("entrar", enterChannel);
+};
 
-document.querySelector("#salir2").onclick = () => {
-    socket.emit("salir", "Web50x.ni", (respuesta) => {
-        document.querySelector("#contenido").append(respuesta)
-        document.querySelector("#contenido").innerHTML += "<br>"
-        document.querySelector("#msg").className = ""
-        document.querySelector("#salir2").setAttribute("hidden", true)
-    })
-}
-
-// Se indica a los otros usuarios la entrada y salida de 
-// los otros usuarios
-socket.on("mensaje", (dato) => {
-    document.querySelector("#contenido").append(dato)
-    document.querySelector("#contenido").innerHTML += "<br>"
-})
-
-*/
-
-
+socket.on("visibilityMsg", function (newCh, oldCh) {
+    document.querySelectorAll(`.${oldCh}`).setAttribute("hidden", "");
+    document.querySelectorAll(`.${newCh}`).removeAttribute("hidden");
+});
 
